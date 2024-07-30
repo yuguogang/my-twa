55401633b8cf3cd9de6b18d8006c1603fddb4199
guogang yu <35683997+yuguogang@users.noreply.github.com>
2024-07-30 15:10:30 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css';
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useCounterContract } from './hooks/useCounterContract';
+import { useTonConnect } from './hooks/useTonConnect';
+
+function App() {
+  const { value, address,sendIncrement } = useCounterContract();
+  const {connected} = useTonConnect();
+  return (
+    <div className='App'>
+      <div className='Container'>
+        <TonConnectButton />
+
+        <div className='Card'>
+          <b>Counter Address</b>
+          <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{value ?? 'Loading...'}</div>
+          <a
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+          onClick={() => {
+            sendIncrement();
+          }}
+        >
+          Increment
+        </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import './index.css'
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
+)
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <TonConnectUIProvider manifestUrl={manifestUrl}>
+//     <App />
+//   </TonConnectUIProvider>,
+// )
